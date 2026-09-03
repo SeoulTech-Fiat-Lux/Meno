2c75ba8bca2407b76021a0261867ee7ea564a6c3
ajbuwidnnfjwj <416tommy@naver.com>
2026-09-03 15:55:58 +0900

    Scene의 object 삭제 기능 추가 #7

--- include/meno/core/Scene.hpp
+++ include/meno/core/Scene.hpp
@@ -6,15 +6,63 @@
 #include <vector>
 #include <memory>
 #include <unordered_map>
+#include <functional>
 
 namespace meno {
 
 class Scene {
 
-    std::unordered_map<GameObjectID, std::unique_ptr<GameObject>> objects_;
+private:
+    std::unordered_map<
+        GameObject::GameObjectID, std::unique_ptr<GameObject>
+    > objects_;
     GameObject::GameObjectID next_id_{1};
 
+    using EnterHandler  = std::function<void()>;
+    using UpdateHandler = std::function<void(float)>;
+    using ExitHandler   = std::function<void()>;
+
+    EnterHandler enter_handler_;
+    UpdateHandler update_handler_;
+    ExitHandler exit_handler_;
+
 public:
+    Scene() = default;
+    ~Scene() = default;
+
+    Scene(const Scene&) = delete;
+    Scene& operator=(const Scene&) = delete;
+
+    Scene(Scene&&) noexcept = default;
+    Scene& operator=(Scene&&) noexcept = default;
+
+    void setEnterHandler(EnterHandler handler) {
+        enter_handler_ = std::move(handler);
+    }
+
+    void setUpdateHandler(UpdateHandler handler) {
+        update_handler_ = std::move(handler);
+    }
+
+    void setExitHandler(ExitHandler handler) {
+        exit_handler_ = std::move(handler);
+    }
+
+    void onEnter() const {
+        if (enter_handler_)
+            enter_handler_();
+    }
+
+    void onUpdate(float dt) const {
+        if (update_handler_)
+            update_handler_(dt);
+    }
+
+    void onExit() const {
+        if (exit_handler_)
+            exit_handler_();
+    }
+
     template <typename T = GameObject, typename... Args>
         requires std::derived_from<T, GameObject>
     T& createGameObject(Args&&... args) {
@@ -25,6 +73,14 @@ public:
 
         return ref;
     }
+
+    [[nodiscard]]
+    GameObject* getGameObject(GameObject::GameObjectID) noexcept;
+    [[nodiscard]]
+    const GameObject* getGameObject(GameObject::GameObjectID) const noexcept;
+
+    [[nodiscard]]
+    bool deleteGameObject(GameObject::GameObjectID) noexcept;
 };
 
 } // namespace meno
--- src/meno/core/Scene.cpp
+++ src/meno/core/Scene.cpp
@@ -1,2 +1,31 @@
 #include <meno/core/Scene.hpp>
+#include <meno/core/GameObject.hpp>
 
+namespace meno {
+
+[[nodiscard]]
+GameObject* Scene::getGameObject(GameObject::GameObjectID id) noexcept {
+    auto it = objects_.find(id);
+
+    if (it == objects_.end())
+        return nullptr;
+
+    return it->second.get();
+}
+
+[[nodiscard]]
+const GameObject* Scene::getGameObject(GameObject::GameObjectID id) const noexcept {
+    auto it = objects_.find(id);
+
+    if (it == objects_.end())
+        return nullptr;
+
+    return it->second.get();
+}
+
+[[nodiscard]]
+bool Scene::deleteGameObject(GameObject::GameObjectID id) noexcept {
+    return objects_.erase(id) != 0;
+}
+
+}
